5467150038312516c47b3e3c726557e7f88937b0
Sam Wu <sam.x.wu@gmail.com>
2017-03-08 05:05:56 +0000

    Checkpoint 4 Mentor Availability Completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -6,16 +6,24 @@ class Kele
     base_uri "https://www.bloc.io/api/v1"
     
     def initialize(email, password)
-         response = self.class.post('/sessions', body: {"email": email, "password": password})
+         response = self.class.post("/sessions", body: {"email": email, "password": password})
          errorHandler(response.code) 
          @auth_token = response['auth_token']
     end
     
     def get_me
-        response = self.class.get('/users/me', headers: { "authorization" => @auth_token })
+        response = self.class.get("/users/me", headers: { "authorization" => @auth_token })
         parsed_response = JSON.parse(response.body)
     end
     
+    def get_mentor_availability(mentor_id)
+        #my_info = get_me()
+        #mentor_id = my_info['current_enrollment']['mentor_id']
+        response = self.class.get("/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token })
+        parsed_response = JSON.parse(response.body)
+    end
+    
+    private
     
     def errorHandler(code)
         raise AuthenticationFailError if code == 401
