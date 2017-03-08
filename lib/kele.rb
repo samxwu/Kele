059bb0e8e44a7b5581852dced241063455b69bca
Sam Wu <sam.x.wu@gmail.com>
2017-03-08 04:39:41 +0000

    Checkpoint 3 Retrieve Users Completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,14 +1,20 @@
 require 'httparty'
+require 'json'
 
 class Kele
     include HTTParty
-    base_uri "https://www.bloc.io/api/v1/sessions"
+    base_uri "https://www.bloc.io/api/v1"
     
     def initialize(email, password)
-         response = self.class.post(self.class.base_uri, body: {"email": email, "password": password})
+         response = self.class.post('/sessions', body: {"email": email, "password": password})
          errorHandler(response.code) 
          @auth_token = response['auth_token']
-    end     
+    end
+    
+    def get_me
+        response = self.class.get('/users/me', headers: { "authorization" => @auth_token })
+        parsed_response = JSON.parse(response.body)
+    end
     
     
     def errorHandler(code)
